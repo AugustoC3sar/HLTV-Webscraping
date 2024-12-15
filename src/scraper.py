127ba9f786261c6ce5407184b392d17293b3fb58
César Augusto <c.augusto0302@gmail.com>
2024-12-15 12:07:45 -0300

    Fluxo de extração concluído. Scraper finalizado

--- src/scraper.py
+++ src/scraper.py
@@ -1,59 +1,239 @@
 import re
+import os
+import datetime
 from threading import Thread
 from time import sleep
-
 
 from src.crawler import Crawler
 from src.dataExtractor import DataExtractor
 from src.dataManager import DataManager
 
+class VLRGGScraper:
+    '''
+        Classe que implementa um WebScraper para o site vlr.gg.
 
-class HLTVScraper:
-    def __init__(self):
-        self.crawler = Crawler("https://hltv.org")
+        Deve extrair, para os primeiros 100 times de cada ranking por região:
+            - Nome do Time
+            - Id do time
+            - Rank do time
+            - Jogadores (se houver)
+            - Técnico (se houver)
+            - Resultados recentes, considerando partidas nos últimos 6 meses (resultado, placar geral (se houver), mapas jogados, placar por mapa)
+            - Estatísticas por mapa (porcetagem de vitórias, porcentagem de rounds vencidos do lado ATAQUE, porcentagem de rounds vencidos do lado DEFESA)
+    '''
+
+    host = "https://vlr.gg"
+
+    def __init__(self, filename):
+        self.crawler = Crawler(self.host)
         self.stop = False
         self.data_extractor = DataExtractor()
-        self.data_manager = DataManager()
-        self.wait_time = 15.0
+        self.data_manager = DataManager(filename)
+        self.wait_time = 5
+        now = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%s")
+        self.log_file = f"scraper_log_{now}"
     
-    def run(self):
-        # Configurando execução periódica do crawler
-        crawler_thread = Thread(target=self.crawlerRun, daemon=True)
-        crawler_thread.start()
+    def log(self, msg):
+        with open(self.log_file, mode="a") as file:
+            file.write(f"{msg}\n")
+        print(f"{msg}")
 
-        # 1. Extrair as URLs dos primeiros 100 times
-        ## Adiciona a url a fila de requisições
-        self.crawler.addToQueue("/ranking/teams")
+    def getResponse(self, url:str):
+        '''
+            Obtém a resposta da requisição realizada para uma dada URL.
 
-        ## Espera pela resposta
+            Retorna o conteúdo (HTML) da resposta, se o status da resposta for OK.
+        '''
         response = None
         while not response:
-            response = self.crawler.getResponse("/ranking/teams")
+            response = self.crawler.getResponse(url)
         
-        ## Extrai as urls dos times
-        urls = self.data_extractor.extractTeamsFromRanking(response)
-
-        for url in urls:
-            self.crawler.addToQueue(url)
-
-        # 2. Acessar cada URL e extrair: Nome do time, jogadores, técnico, posição no ranking
-        for url in urls:
-            response = None
-            while not response:
-                response = self.crawler.getResponse(url)
-            
-            team_id = re.search(r"\d+", url).group(0)
-            name, players, rank, coach = self.data_extractor.extractTeamInfo(response)
-            self.data_manager.addNewTeam(team_id, name, players, rank, coach)
-            self.data_manager.exportJson()
+        if response.status_code != 200:
+            return None
         
-        # 3. Acessar a página de resultado de cada time, filtrando pelos últimos 3 meses, e extrair a URL de todas as partidas
+        return response.content
 
-        # 4. Acessar cada partida e extrair os vetos
+    def run(self):
+        '''
+            Inicializa o WebScraper, configurando uma Thread dedicada para o crawler e
+            efetuando o loop de execução para cada ranking.
+        '''
+        # Configurando thread crawler
+        for _ in range(5):
+            crawler_thread = Thread(target=self.crawlerRun, daemon=True)
+            crawler_thread.start()
+
+        # 1. Extraindo a lista de rankings por região
+        self.crawler.addRankingToQueue("/rankings")
+
+        ## Obtém a resposta da requisição
+        response = self.getResponse("/rankings")
+        if not response:
+            self.log("Invalid response for URL '/rankings'! (ABORTING)")
+            return -1
+
+        ## O método retorna uma lista com a url de todos os rankings por região
+        self.log("Extracting Regional Rankings URLs...")
+        try:
+            rankings = self.data_extractor.extractRegionalRankingsPaths(response)
+        except Exception as e:
+            self.log(f"Regional Rankings URLs extraction FAILED with error {e}! (ABORTING)")
+            return -1
+
+        #rankings = ["/rankings/brazil"]
+
+        # 2. Adicionando o link dos rankings na fila de requisições
+        self.log("Adding rankings URLs to queue...")
+        for ranking in rankings:
+            self.crawler.addRankingToQueue(ranking)
+
+        # 3. Para cada ranking, executa o loop
+        for ranking in rankings:
+            self.log("="*70)
+            self.log(f"Starting ranking '{ranking}' data extraction...")
+
+            code = self.mainloop(ranking)
+            if code == -1:
+                self.log(f"Data extraction for ranking '{ranking}' finished with errors!")
+                continue
+
+            self.log(f"Data extraction for ranking '{ranking}' successfully finished!")
+        self.log("="*70)
         
+        self.data_manager.save_data()
         self.stop = True
-    
+
+        return 0
+
+    def mainloop(self, ranking):
+        '''
+            Método principal do sistema.
+
+            Para cada ranking:
+                - Extrai os times e a região do ranking
+                - Para os 100 primeiros times do ranking
+                    - Extrai as informações citadas na descrição da classe
+        '''
+        return_code = 0
+
+        # 1. Espera pela resposta da requisição ao ranking
+        response = self.getResponse(ranking)
+        if not response:
+            self.log(f"Invalid response for URL '{ranking}'! (SKIPPING RANKING)")
+            return -1
+        
+        # 2. Extrai as URLs dos times
+        self.log(f"Extracting Teams URLs from ranking '{ranking}'...")
+        try:
+            urls, region = self.data_extractor.extractTeamsFromRanking(response)
+        except Exception as e:
+            self.log(f"Teams URLs extraction from ranking '{ranking}' FAILED with error {e}! (SKIPPING RANKING)")
+            return -1
+
+        # 3. Adiciona as URLs na fila de requisições
+        for url in urls:
+            self.crawler.addTeamToQueue(url)
+
+        # 4. Para cada URL (time) nos 100 primeiros times
+        rank = 0
+
+        for url in urls:
+            team_ulrs = [self.host+ranking, self.host+url]
+            rank += 1
+
+            self.log("-"*70)
+            self.log(f"Starting team '{url}' data extraction...")
+
+            # 4.1 Espera pela resposta da requisição da página do time
+            response = self.getResponse(url)
+            if not response:
+                self.log(f"Invalid response for URL '{url}'! (SKIPPING TEAM)")
+                return_code = -1
+                continue
+
+            # 4.2 Extrai o ID do time da URL
+            team_id = re.search(r"\d+", url).group(0)
+            if self.data_manager.datasetHasTeam(team_id):
+                self.log(f"Team '{team_id}' already on the dataset! (SKIPPING TEAM)")
+                continue
+
+            # 4.3 Extrai nome, jogadores e head coach do time
+            self.log(f"Extracting team '{url}' info...")
+            try:
+                name, players, coach = self.data_extractor.extractTeamInfo(response)
+            except Exception as e:
+                self.log(f"Team '{url}' info extraction FAILED with error {e}! (SKIPPING TEAM)")
+                return_code = -1
+                continue
+
+            # 4.4 Extrai a URL para a página com a lista de partidas do time e adiciona na fila de requisições
+            self.log(f"Extracting team '{url}' matchlist page URL...")
+            try:
+                matchlist_page = self.data_extractor.extractTeamMatchlistPage(response)
+            except Exception as e:
+                self.log(f"Team '{url}' matchlist page URL extraction FAILED with error {e}! (SKIPPING TEAM)")
+                return_code = -1
+                continue
+            self.crawler.addTeamPageToQueue(matchlist_page)
+
+            # 4.5 Extrai a URL para a página de estatísticas do time e adiciona na fila de requisições
+            self.log(f"Extracting team '{url}' statistics page URL...")
+            try:
+                stats_page = self.data_extractor.extractTeamStatsPage(response)
+            except Exception as e:
+                self.log(f"Team '{url}' statistics page URL extraction FAILED with error {e}! (SKIPPING TEAM)")
+                return_code = -1
+                continue
+            self.crawler.addTeamPageToQueue(stats_page)
+
+            # 4.5 Espera pela resposta da requisição da página de lista de partidas do time
+            response = self.getResponse(matchlist_page)
+            if not response:
+                self.log(f"Invalid response for URL '{matchlist_page}'! (SKIPPING TEAM)")
+                return_code = -1
+                continue
+            team_ulrs.append(self.host+matchlist_page)
+            
+            # 4.6 Extrai os resultados recentes do time
+            self.log(f"Extracting team '{url}' recent matches results...")
+            try:
+                recent_results = self.data_extractor.extractTeamRecentMatchesResult(response)
+            except Exception as e:
+                self.log(f"Team '{url}' recent matches results extracion FAILED with error {e}! (SKIPPING TEAM)")
+                return_code = -1
+                continue
+
+            # 4.7 Espera pela resposta da requisição da página de estatísticas do time
+            response = self.getResponse(stats_page)
+            if not response:
+                self.log(f"Invalid response for URL '{stats_page}'! (SKIPPING TEAM)")
+                return_code = -1
+                continue
+            team_ulrs.append(self.host+stats_page)
+
+            self.log(f"Extracting team '{url}' maps statistics...")
+            try:
+                maps_stats = self.data_extractor.extractTeamMapsStats(response)
+            except Exception as e:
+                self.log(f"Team '{url}' maps statistics extraction FAILED with error {e}! (SKIPPING TEAM)")
+                return_code = -1
+                continue
+
+            # 4.8 Salva o time na na lista de times
+            self.data_manager.addNewTeam(team_id, name, players, region, coach, rank, recent_results, maps_stats, urls)
+            self.log(f"Team '{name}' added to dataset!")
+
+        self.log("-"*70)
+
+        return return_code
+                
     def crawlerRun(self):
+        '''
+            Método executado pela thread dedicada do crawler.
+
+            Chama o método que efetua o download de uma página do crawler e
+            espera por um tempo predefinido antes de efetuar um novo download.
+        '''
         while not self.stop:
             self.crawler.requestPage()
             sleep(self.wait_time)
