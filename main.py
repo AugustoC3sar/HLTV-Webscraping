bc83ef4a24e3debedfaab0c61acfd60c277ca7b8
César Augusto <c.augusto0302@gmail.com>
2024-12-15 12:08:11 -0300

    Adição de argumento para nomear arquivo de saída

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from src.scraper import HLTVScraper
+import argparse
+
+from src.scraper import VLRGGScraper
 
 if __name__ == "__main__":
-    app = HLTVScraper()
-    app.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename", required=False, type=str, default="./VLRGG_Scraping_Dataset.json", help="The name of the output file with data collected by the WebScraper")
+    args = parser.parse_args()
+
+    app = VLRGGScraper(args.filename)
+    code = app.run()
+
+    exit(code)
