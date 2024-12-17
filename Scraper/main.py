99edba55b11e0f06f6d710ed24f1b153a40a1921
César Augusto <c.augusto0302@gmail.com>
2024-12-16 21:37:55 -0300

    Ajuste no nome default do arquivo final

--- Scraper/main.py
+++ Scraper/main.py
@@ -4,7 +4,7 @@ from src.scraper import VLRGGScraper
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--filename", required=False, type=str, default="./VLRGG_Scraping_Dataset.json", help="The name of the output file with data collected by the WebScraper")
+    parser.add_argument("-f", "--filename", required=False, type=str, default="./VLRGG_Scraper_Dataset.json", help="The name of the output file with data collected by the WebScraper")
     args = parser.parse_args()
 
     app = VLRGGScraper(args.filename)
